04a7dc8e1b7fd90e1d0c56a1566df5eb65724518
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-23 22:45:51 -0600

    meh. not working.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 console.log("hi");
+//Clockwork API key: 05402f56c48915b1d4058a6ec0f8fe75be8d57ce
+
 // $.ajax({
 //   // url: "http://textbelt.com/text?number=9132169521?message=hi",
 //   url: "http://textbelt.com/text",
@@ -10,8 +12,10 @@ console.log("hi");
 //     console.log(data);
 // });
 var g =  $.post("http://textbelt.com/text",
+  // var g = $.post("http://text@textbelt.com/text",
 {
-  number: 9132169521,
+  // number: 9132169521,
+  number: 8592277295,//deric
   message: "testing https://github.com/typpo/textbelt"
 }
 ).done(function (data){
